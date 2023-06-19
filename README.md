f17ab1ab01dc4be4b9b29b3b5cad5b348cd86fe6
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2023-06-19 20:56:01 +0100

    UE2 & UE4 Batch Scripts
    First complete version of the batch scripts that prepare all of the files for importing SGA into UE2 and or UE4.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # UE2-UE4
 A batch script I am working on that prepares all of the files for a port I am working on.
+UE4 ( for UE4 version only ) - Not actually required yet!
+UT2004 ( required to export the sound packages )
+UModel from https://www.gildor.org/en/projects/umodel
+A copy of "Stargate_SG-1_-_The_Alliance-2005-12-15" which can be obtained from https://archive.org/details/StargateSG1TheAlliance20051215
+FFMPEG from https://ffmpeg.org/
+
+Install UE4 ( if required )
+
+Install UT2004
+
+Install FFMPEG to C:\ffmpeg
+( so "C:\ffmpeg\bin" will now exist )
+
+Extract umodel and copy it to C:\umodel_win32
+( so "C:\umodel_win32\UmodelExport" will now exist )
+
+Extract the game files to C:\SGA
+( so "C:\SGA\Textures" will now exist )
+
+Copy the script to "C:\umodel_win32" and run it from here.
+
+This will setup all of the files appropriately. ( Animations / StaticMeshes still remain!!! )
+
+It will also delete the C:\SGA directory upon completion, so you can just keep or delete the original .7z file
---
+++ sga-ue2-release.bat
@@ -0,0 +1,52 @@
+del C:\SGA\Textures\M01Tex.utx C:\SGA\Textures\M05ATex.utx C:\SGA\Textures\M05BTex.utx C:\SGA\Textures\M05CTex.utx C:\SGA\Textures\M05dTex.utx C:\SGA\Textures\M08Tex.utx C:\SGA\Textures\M10Atex.utx C:\SGA\Textures\M10Btex.utx C:\SGA\Textures\M12Tex_test.utx C:\SGA\Textures\M15Tex22.utx C:\SGA\Textures\M15Tex_tomerge.utx C:\SGA\Textures\m16.utx C:\SGA\Textures\M16ATex.utx C:\SGA\Textures\M16BTex.utx C:\SGA\Textures\StargateEffectsTex.utx C:\SGA\Textures\M10_train_anim.ukx C:\SGA\Textures\SGAllianceUIX.uix C:\SGA\Textures\UE2Runtime.ini C:\SGA\Textures\XBoxLiveFont.xpr C:\SGA\Textures\m17tex.utx C:\SGA\Textures\x_delete_m17tex.utx C:\SGA\Textures\xx_old_m17tex.utx C:\SGA\Textures\M07_fresco_tex.utx C:\SGA\Textures\M10_train_tex.utx C:\SGA\Textures\StargateGameFontsTex.utx C:\SGA\Textures\Tutorial_Tex.utx
+
+mkdir "C:\GOG Games\Unreal Tournament 2004\Speech\"
+
+umodel -path=C:\SGA\Animations -export *.ukx
+
+for /f %%f in ('dir /b C:\umodel_win32\UmodelExport\') do move C:\umodel_win32\UmodelExport\%%f "C:\GOG Games\Unreal Tournament 2004\Animations\%%f"
+
+for /f %%f in ('dir /b C:\SGA\Music\') do move C:\SGA\Music\%%f "C:\GOG Games\Unreal Tournament 2004\Music\%%f"
+
+for /f %%f in ('dir /b C:\SGA\Speech\') do move C:\SGA\Speech\%%f "C:\GOG Games\Unreal Tournament 2004\Speech\%%f"
+
+umodel -path=C:\SGA\StaticMeshes -export *.usx
+
+for /D %%D in ("C:\umodel_win32\UmodelExport\*") do (
+    for %%F in ("%%~D\StaticMesh\*.pskx*") do (
+        move /Y "%%~F" "%%~dpF.."
+    )
+)
+
+FOR /d /r . %%d IN (StaticMesh,Shader,Texture,TexEnvMap) DO @IF EXIST "%%d" rd /s /q "%%d"
+
+for /f "delims=|" %%f in ('dir /b C:\umodel_win32\UmodelExport\') do move "C:\umodel_win32\UmodelExport\%%f" "C:\GOG Games\Unreal Tournament 2004\StaticMeshes\%%f"
+
+umodel -path=C:\SGA\Textures -export *.utx
+for /D %%D in ("C:\umodel_win32\UmodelExport\*") do (
+    for %%F in ("%%~D\Texture\*.tga*") do (
+        move /Y "%%~F" "%%~dpF.."
+    )
+)
+
+FOR /d /r . %%d IN (Texture,Shader,TexEnvMap,TexPanner,Combiner,FinalBlend,TexOscillator,TexRotator,TexScaler,StaticMesh,VertMesh) DO @IF EXIST "%%d" rd /s /q "%%d"
+
+cd "C:\GOG Games\Unreal Tournament 2004\System"
+
+for /f %%f in ('dir /b C:\umodel_win32\UmodelExport\') do ucc pkg import texture %%f C:\umodel_win32\UmodelExport\%%f
+for /r "C:\GOG Games\Unreal Tournament 2004\System" %%x in (*.utx) do move "%%x" "C:\GOG Games\Unreal Tournament 2004\Textures"
+rmdir /q /s C:\umodel_win32\UmodelExport\
+mkdir C:\umodel_win32\UmodelExport\
+
+for /f "usebackq delims=|" %%f in (`dir /b "C:\SGA\Sounds\"`) do ucc batchexport C:\SGA\Sounds\%%f sound wav C:\umodel_win32\UmodelExport\%%~nf
+
+for /f "usebackq delims=|" %%f in (`dir /b "C:\umodel_win32\UmodelExport\"`) do "C:\ffmpeg\bin\ffmpeg" "%%f" "%%f"
+
+for /f %%f in ('dir /b C:\umodel_win32\UmodelExport\') do ucc pkg import sound %%f C:\umodel_win32\UmodelExport\%%f
+for /r "C:\GOG Games\Unreal Tournament 2004\System" %%x in (*.uax) do move "%%x" "C:\GOG Games\Unreal Tournament 2004\Sounds"
+rmdir /q /s C:\umodel_win32\UmodelExport\
+mkdir C:\umodel_win32\UmodelExport\
+
+rmdir /q /s C:\SGA
+
+pause
