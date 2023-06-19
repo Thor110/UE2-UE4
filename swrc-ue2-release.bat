043b8828f9209601a81367f152da636482dcaa95
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2023-06-20 00:08:19 +0100

    SWRC to UE2 Sound Import Fixed

--- swrc-ue2-release.bat
+++ swrc-ue2-release.bat
@@ -40,6 +40,8 @@ cd "C:\SWRC\System"
 
 for /f "usebackq delims=|" %%f in (`dir /b "C:\SWRC\Sounds\"`) do ucc batchexport C:\SWRC\Sounds\%%f sound wav C:\umodel_win32\UmodelExport\%%~nf
 
+cd "C:\GOG Games\Unreal Tournament 2004\System"
+
 for /f %%f in ('dir /b C:\umodel_win32\UmodelExport\') do ucc pkg import sound %%f C:\umodel_win32\UmodelExport\%%f
 for /r "C:\GOG Games\Unreal Tournament 2004\System" %%x in (*.uax) do move "%%x" "C:\GOG Games\Unreal Tournament 2004\Sounds"
 rmdir /q /s C:\umodel_win32\UmodelExport\
