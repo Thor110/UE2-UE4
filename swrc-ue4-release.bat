e9179181263a5dfbbe32cc774b3cfeb386106020
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-02-15 22:07:31 +0000

    Duplicate Not Needed

--- swrc-ue4-release.bat
+++ swrc-ue4-release.bat
@@ -8,14 +8,12 @@ set first=%cd%
 cls
 REM enter directories of required programs
 :UNREAL
-SET /P level="Enter the duplicated Star Wars Republic Commando GameData Directory:"
+SET /P level="Enter your Star Wars Republic Commando GameData Directory:"
 if exist "%level%\System\UCC.exe" (
   echo UCC Found.
 ) else (
   echo UCC Not Found!
   goto :UNREAL
-  pause
-  exit
 )
 
 :UMODEL
@@ -25,8 +23,6 @@ if exist "%model%\umodel.exe" (
 ) else (
   echo UModel Not Found!
   goto :UMODEL
-  pause
-  exit
 )
 
 :BLENDER
@@ -36,8 +32,6 @@ if exist "%blend%\blender.exe" (
 ) else (
   echo Blender Not Found!
   goto :BLENDER
-  pause
-  exit
 )
 
 :EXPORT
@@ -48,27 +42,33 @@ if exist "%start%" (
 ) else (
   echo Content Folder Not Found!
   goto :EXPORT
-  pause
-  exit
 )
 
 REM make directories
-mkdir "%start%\Materials" "%start%\StaticMeshes" %start%\Sounds %start%\Animations %start%\Music\ %start%\Splash\ %start%\Game\ %start%\Movies\
+mkdir "%start%\Materials" "%start%\StaticMeshes" "%start%\Sounds" "%start%\Animations" "%start%\Music" "%start%\Splash" "%start%\Game" "%start%\Movies"
 
 Rem these packages do not contain any sounds
-del %files%\Sounds\banter_voice.uax %files%\Sounds\params_mus.uax %files%\Sounds\params_rumble.uax %files%\Sounds\params_sfx.uax %files%\Sounds\params_vox.uax
+REM del %files%\Sounds\banter_voice.uax %files%\Sounds\params_mus.uax %files%\Sounds\params_rumble.uax %files%\Sounds\params_sfx.uax %files%\Sounds\params_vox.uax
+
+REM make temporary directory and move the packages that do not contain any sounds
+mkdir "%level%\Temporary"
+move /Y "%level%\Sounds\banter_voice.uax" "%level%\Temporary"
+move /Y "%level%\Sounds\params_mus.uax" "%level%\Temporary"
+move /Y "%level%\Sounds\params_rumble.uax" "%level%\Temporary"
+move /Y "%level%\Sounds\params_sfx.uax" "%level%\Temporary"
+move /Y "%level%\Sounds\params_vox.uax" "%level%\Temporary"
 
 REM change directory to umodel
-cd /d %model%
+cd /d "%model%"
 
 REM export animations packages with umodel
 umodel -path="%level%\Animations" -export *.ukx
 
 REM for all directories in the umodelexport folder move to the UE4 animations folder
-for /f %%f in ('dir /b %model%\UmodelExport\') do move "%model%\UmodelExport\%%f" "%start%\Animations\%%f"
+for /f %%f in ('dir /b "%model%\UmodelExport"') do move "%model%\UmodelExport\%%f" "%start%\Animations\%%f"
 
 REM for all directories in the music folder of the game move to the UE4 music folder
-for /f %%f in ('dir /b "%level%\Music\"') do move "%level%\Music\%%f" %start%\Music\%%f
+for /f %%f in ('dir /b "%level%\Music"') do move "%level%\Music\%%f" "%start%\Music\%%f"
 
 REM export texture packages with umodel
 umodel -path="%level%\Textures" -export *.utx
@@ -88,7 +88,7 @@ REM removed for now
 Rem FOR /d /r . %%d IN (Texture,Shader,TexEnvMap,TexPanner,Combiner,FinalBlend,TexOscillator,TexRotator,TexScaler,StaticMesh,VertMesh) DO @IF EXIST "%%d" rd /s /q "%%d"
 
 REM for every directory in the umodelexport folder move to the UE4 materials folder
-for /f "delims=|" %%f in ('dir /b %model%\UmodelExport\') do move "%model%\UmodelExport\%%f" "%start%\Materials\%%f"
+for /f "delims=|" %%f in ('dir /b "%model%\UmodelExport"') do move "%model%\UmodelExport\%%f" "%start%\Materials\%%f"
 
 REM export staticmesh packages with umodel
 umodel -path="%level%\StaticMeshes" -export *.usx
@@ -108,32 +108,32 @@ REM removed for now
 Rem FOR /d /r . %%d IN (StaticMesh,Shader,Texture,TexEnvMap,MeshAnimation,SkeletalMesh) DO @IF EXIST "%%d" rd /s /q "%%d"
 
 REM for every directory in the umodelexport folder move to the UE4 StaticMeshes folder
-for /f "delims=|" %%f in ('dir /b %model%\UmodelExport\') do move "%model%\UmodelExport\%%f" "%start%\StaticMeshes\%%f"
+for /f "delims=|" %%f in ('dir /b "%model%\UmodelExport"') do move "%model%\UmodelExport\%%f" "%start%\StaticMeshes\%%f"
 
 REM change directory to the SWRC System folder
 cd /d "%level%\System"
 
 REM for every file in the Sounds folder do batchexport with ucc 
-for /f "usebackq delims=|" %%f in (`dir /b "%level%\Sounds\"`) do ucc batchexport "%level%\Sounds\%%f" sound wav %start%\Sounds\%%~nf
+for /f "usebackq delims=|" %%f in (`dir /b "%level%\Sounds\"`) do ucc batchexport "%level%\Sounds\%%f" sound wav "%start%\Sounds\%%~nf"
 
 REM line no longer used
 REM for /f %%f in ('dir /b %model%\UmodelExport\') do move %model%\UmodelExport\%%f %start%\Sounds\%%f
 
 REM change directory to original directory
-cd %first%
+cd "%first%"
 
 REM delete the duplicate game folder removed for now
 REM rmdir /q /s %level%
 
 Rem this mesh fails to convert to fbx so it is deleted
-del %start%\StaticMeshes\markericons\TrapXSpotIcon.pskx
+del "%start%\StaticMeshes\markericons\TrapXSpotIcon.pskx"
 
 REM disable delayed expansion
 setlocal disableDelayedExpansion
 
 REM set Input/Output/Find/Insert strings
-set InputFile=%first%\batch-convert-fbx.txt
-set OutputFile=%blend%\batch-convert-fbx.py
+set "InputFile=%first%\batch-convert-fbx.txt"
+set "OutputFile=%blend%\batch-convert-fbx.py"
 set "_strFind=path = 'C:\'"
 set "_strInsert=path = '%start%'"
 
@@ -149,13 +149,13 @@ for /f "usebackq delims=" %%A in ("%InputFile%") do (
 )
 
 REM change directory to the blender directory
-cd /d %blend%
+cd /d "%blend%"
 
 REM batch convert psk/pskx/psa to FBX with blender
 blender -b -P batch-convert-fbx.py
 
 REM delete the following filetypes from the StaticMeshes & Animations folders in the UE4 directory ( .pskx, .psk, .psa, .config )
-del /S %start%\StaticMeshes\*.pskx %start%\StaticMeshes\*.psk %start%\StaticMeshes\*.psa %start%\StaticMeshes\*.config %start%\Animations\*.psk %start%\Animations\*.psa %start%\Animations\*.config
+del /S "%start%\StaticMeshes\*.pskx" "%start%\StaticMeshes\*.psk" "%start%\StaticMeshes\*.psa" "%start%\StaticMeshes\*.config" "%start%\Animations\*.psk" "%start%\Animations\*.psa" "%start%\Animations\*.config"
 
 REM for every folder in the UE4 Animations directory
 for /D %%D in ("%start%\Animations\*") do (
@@ -172,10 +172,18 @@ REM line no longer needed
 REM cd /d %start%\Animations\
 
 REM delete directory that contains files that exist in the Materials folder already
-rd /s /q %start%\Animations\bactadispensers\BactaDispenserGEO
+rd /s /q "%start%\Animations\bactadispensers\BactaDispenserGEO"
 
 REM line no longer used
 Rem FOR /d /r . %%d IN (SkeletalMesh,Texture,MeshAnimation,VertMesh,Shader) DO @IF EXIST "%%d" rd /s /q "%%d"
+
+REM move sound packages back and delete temporary directory
+move /Y "%level%\Temporary\banter_voice.uax" "%level%\Sounds"
+move /Y "%level%\Temporary\params_mus.uax" "%level%\Sounds"
+move /Y "%level%\Temporary\params_rumble.uax" "%level%\Sounds"
+move /Y "%level%\Temporary\params_sfx.uax" "%level%\Sounds"
+move /Y "%level%\Temporary\params_vox.uax" "%level%\Sounds"
+rd /s /q "%level%\Temporary"
 
 pause
 REM pause and exit for now
